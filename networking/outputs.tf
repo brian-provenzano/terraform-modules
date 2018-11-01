65f53cfa9630bb844b58507d4e5d915f1c4fe733
Brian Provenzano <bproven@gmail.com>
2018-10-31 18:20:55 -0600

    refactor: reorganizing, removing security groups from networking module, remove output specific to sgs from networking module

--- networking/outputs.tf
+++ networking/outputs.tf
@@ -2,20 +2,8 @@ output "vpc_id" {
   value = "${aws_vpc.main.id}"
 }
 
-output "clusterweb_security_group_id" {
-  value = "${aws_security_group.ssh_web_instance_cluster.id}"
-}
-
-output "clusterweb_elb_security_group_id" {
-  value = "${aws_security_group.webservers_elb.id}"
-}
-
 output "webserver_port" {
   value = "${var.webserver_port}"
-}
-
-output "bastion_security_group_id" {
-  value = "${aws_security_group.ssh_bastion_instance.id}"
 }
 
 output "bastion_elasticip_id" {
@@ -24,22 +12,6 @@ output "bastion_elasticip_id" {
 
 output "bastion_elasticip" {
   value = "${element(split(",", join(",", aws_eip.bastion.*.public_ip)), 0)}"
-}
-
-output "rds_security_group_id" {
-  value = "${aws_security_group.rds_instance.id}"
-}
-
-output "jenkinsmaster_security_group_id" {
-  value = "${aws_security_group.jenkinsmaster_instance.id}"
-}
-
-output "jenkinsslave_security_group_id" {
-  value = "${aws_security_group.jenkinsslave_instance.id}"
-}
-
-output "simpleweb_security_group_id" {
-  value = "${aws_security_group.ssh_web_instance.id}"
 }
 
 output "public_subnets" {
@@ -58,7 +30,36 @@ output "database_subnet_group_id" {
   value = "${element(split(",", join(",", aws_db_subnet_group.database_group.*.id)), 0)}"
 }
 
-
 output "database_subnet_group_name" {
   value = "${element(split(",", join(",", aws_db_subnet_group.database_group.*.name)), 0)}"
 }
+
+
+# output "clusterweb_security_group_id" {
+#   value = "${aws_security_group.ssh_web_instance_cluster.id}"
+# }
+
+# output "clusterweb_elb_security_group_id" {
+#   value = "${aws_security_group.webservers_elb.id}"
+# }
+
+# output "bastion_security_group_id" {
+#   value = "${aws_security_group.ssh_bastion_instance.id}"
+# }
+
+# output "rds_security_group_id" {
+#   value = "${aws_security_group.rds_instance.id}"
+# }
+
+# output "jenkinsmaster_security_group_id" {
+#   value = "${aws_security_group.jenkinsmaster_instance.id}"
+# }
+
+# output "jenkinsslave_security_group_id" {
+#   value = "${aws_security_group.jenkinsslave_instance.id}"
+# }
+
+# output "simpleweb_security_group_id" {
+#   value = "${aws_security_group.ssh_web_instance.id}"
+# }
+
